4e1a5fefff85ce6ccb849d273f19022024af8fe0
Walteann <walteann3@gmail.com>
2021-12-12 13:08:29 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,15 @@ import { createConnection } from 'typeorm';
 import { globalErrors } from './middlewares/globalErrors';
 import routes from './routes';
 
+import cors from 'cors';
+
 createConnection()
   .then(connection => {
     const app = express();
     const PORT = 3333;
 
 
+  app.use(cors());
 	app.use(express.json());
     app.use(routes);
     app.use(globalErrors);
